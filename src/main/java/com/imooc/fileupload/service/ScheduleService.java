cce1447c88caf2ce32608122b858808bc2353bb8
limou <1121136571@qq.com>
2019-06-01 17:28:40 +0800

    remove quartz and security

--- src/main/java/com/imooc/fileupload/service/ScheduleService.java
+++ src/main/java/com/imooc/fileupload/service/ScheduleService.java
@@ -1,16 +1,12 @@
 package com.imooc.fileupload.service;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 @Service
 public class ScheduleService {
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void job(){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String dateStr = sdf.format(new Date());
-        System.out.println("=========" + dateStr);
-    }
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void job(){
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String dateStr = sdf.format(new Date());
+//        System.out.println("=========" + dateStr);
+//    }
 }
